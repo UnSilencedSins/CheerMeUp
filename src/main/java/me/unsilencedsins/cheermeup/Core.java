3b8df7e35d45682a629134ff8bec8f4bc96bf0ec
Jeremy Laing <jeremy.r.laing@gmail.com>
2020-03-09 10:49:04 -0400

    added title support

--- src/main/java/me/unsilencedsins/cheermeup/Core.java
+++ src/main/java/me/unsilencedsins/cheermeup/Core.java
@@ -1,30 +1,41 @@
 package me.unsilencedsins.cheermeup;
 
+import me.unsilencedsins.api.Titles;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
 
-public final class Core extends JavaPlugin implements CommandExecutor {
-    private boolean onJoin = false;
+public final class Core extends JavaPlugin implements CommandExecutor, Listener {
+    private boolean onJoin;
+    private boolean showTitle;
 
 
     @Override
     public void onEnable() {
         getConfig().options().copyDefaults(true);
         saveDefaultConfig();
+
+        onJoin = getConfig().getBoolean("onJoin", true);
+        showTitle = getConfig().getBoolean("showAsTitle", false);
+
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e){
@@ -35,15 +46,17 @@ public final class Core extends JavaPlugin implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
 
-        if (command.getName().equalsIgnoreCase("cheermeup")) //want a message
-            player.sendMessage(ChatColor.GREEN + getMessage());
-        else if (command.getName().equalsIgnoreCase("cheermeupversion")) //gets the version
-            player.sendMessage(("Current CheerMeUp Version: " + getDescription().getVersion()));
-        else if (command.getName().equalsIgnoreCase("cheermeupreload") &&
-                player.hasPermission("cheermeup.reload")){
-            reloadConfig();
-            player.sendMessage(ChatColor.GREEN + "Config Reloaded");
+        if (command.getName().equalsIgnoreCase("cheermeup")){ //want a message
+            if(!showTitle) {
+                player.sendMessage(getMessage());
+                return true;
+            }
+
+            Titles.sendTitle(player, 20, 60, 20, getMessage(), " ");
         }
+        else if (command.getName().equalsIgnoreCase("cheermeupversion")) //gets the version
+            player.sendMessage((ChatColor.GREEN + "" + ChatColor.BOLD + "Current CheerMeUp Version: " +
+                    getDescription().getVersion()));
 
         return true;
     }
@@ -56,7 +69,10 @@ public final class Core extends JavaPlugin implements CommandExecutor {
     public String getMessage(){
         List<String> messages = getMessagesList();
 
-        if (messages.size() > 0) return messages.get((int) (Math.random() * messages.size()));
+        if (messages.size() > 0)
+        {
+            return ChatColor.GREEN + messages.get((int) (Math.random() * messages.size()));
+        }
 
         return "Tell the admins to add some nice things to say!";
     } //sends a message
